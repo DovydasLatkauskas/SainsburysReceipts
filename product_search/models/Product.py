2c7984f68092ddd0f9404799d4f494729baf63a7
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-03-04 14:55:13 +0000

    created models for Product and Receipt

---
+++ product_search/models/Product.py
@@ -0,0 +1,23 @@
+class Product:
+    # instance variables
+    id : int # should be created by the database??
+    price : int
+    name_on_receipt : str
+    name_on_website : str
+    image_link : str
+    # category info?
+    # nutritional info?
+
+    # def __init__(self): # uncomment if we want to create an empty instance of Product
+    #     pass
+
+    def __init__(self, price, name_on_receipt): # data received from receipt
+        self.price = price
+        self.name_on_receipt = name_on_receipt
+
+    # if we need getters and setters we can make them later
+    # def get_price(self):
+    #     return self.price
+    
+    # def set_price(self, new_price):
+    #     self.price = new_price
---
+++ product_search/models/Receipt.py
@@ -0,0 +1,17 @@
+from Product import Product
+import datetime
+
+class Receipt:
+    id : int # should be created by the database??
+    products : [Product]
+    datetime : datetime.datetime
+
+    def __init__(self, products, datetime):
+        self.products = products
+        self.datetime = datetime
+
+    def get_products(self):
+        return self.products
+    
+    def get_datetime(self):
+        return self.datetime
