8d96e286d5467ec56a1396d1cdcb9ecd41e6b7c9
Jeremy Foxcroft <foxcroftjn@gmail.com>
2023-03-04 15:12:51 +0000

    Show a product table

---
+++ script.js
@@ -0,0 +1,16 @@
+async function getProducts() {
+    return [
+        {
+            name: "Waitrose Essential Oatmilk",
+            price: 1.99,
+            quantity: 1,
+            image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/4/LN_718674_BP_4.jpg",
+        },
+        {
+            name: "Oat Milk Hot Chocolate",
+            price: 2.99,
+            quantity: 1,
+            image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
+        },
+    ];
+}
