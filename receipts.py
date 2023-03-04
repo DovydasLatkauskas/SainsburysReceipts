4e44d25899455a8ceb06a4024add166dfeeff897
Marty-18 <99477330+Marty-18@users.noreply.github.com>
2023-03-04 15:43:55 +0000

    item list and price

--- receipts.py
+++ receipts.py
@@ -1,9 +1,11 @@
 from veryfi import Client
 from abc import ABC
 import json
+import os
+
 
 class VerifyParser():
-    def __init__(self,filename: str) -> None:
+    def __init__(self, filename: str) -> None:
         self.client_id = 'vrfB0juUkBA4LwC5vwGrBUhwboiuwIMPRU68LTB'
         self.client_secret = '3Dnhw0PwbAkX3gtJa3iAJHuxKGsYUU1cgAE4y6e5cjyaUsvhyjUJuJOWoaccKTPH8jxyiVSTh7JkrPefMlz3Ac70u0oY3POTRyozlCbaNSAydG6ZN1UU2G7wHtwNkWzc'
         self.username = 'marlycef4'
@@ -13,33 +15,40 @@ class VerifyParser():
         self.items = None
         self.response = None
 
-
-
     def get_response(self) -> None:
-        veryfi_client = Client(self.client_id, 
-                                self.client_secret, 
-                                self.username, 
-                                self.api_key)
-        
-        response = veryfi_client.process_document(self.file_path, 
-                                                    categories=self.categories)
-        self.response = response
-        self.items = response["line_items"]
+        if os.path.isfile("response.json"):
+            response = json.loads("response.json")
+        else:
+            veryfi_client = Client(self.client_id,
+                                   self.client_secret,
+                                   self.username,
+                                   self.api_key)
+
+            response = veryfi_client.process_document(self.file_path,
+                                                      categories=self.categories)
+            self.response = response
+            json.dumps(self.response)
 
     def get_date(self) -> str:
         if self.items == None:
             self.get_response()
-        
+
         return self.response["created_date"]
-    
+
     def get_line_items(self):
-        pass
+        if self.items == None:
+            self.get_response()
+
+        self.items = list()
+        for item in self.response["line_items"]:
+            self.items.append(tuple([item['description'], item['total']]))
+
+        return self.items
 
 
+filename = 'receipts_test.jpg'
+parser = VerifyParser(filename)
+items = parser.get_line_items()
+print(items)
 
-
-parser = VerifyParser('bad receipt.jpg')
-date = parser.get_date()
-print(date)
-
-print(parser.response)
+# print(parser.response)
