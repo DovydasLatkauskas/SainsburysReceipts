5374d278e20da3a0ac92425a2feb2ff5fcbf65d7
Jeremy Foxcroft <foxcroftjn@gmail.com>
2023-03-04 18:43:17 +0000

    Accept receipts instead of products

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-async function getProducts() {
+async function getReceipts() {
     return [
         {
-            name: "Waitrose Essential Oatmilk",
-            price: 1.99,
-            quantity: 1,
-            image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/4/LN_718674_BP_4.jpg",
+            receipt_id: 123,
+            line_items: [
+                {
+                    name: "Waitrose Essential Oatmilk",
+                    price: 1.99,
+                    quantity: 1,
+                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/4/LN_718674_BP_4.jpg",
+                },
+                {
+                    name: "Oat Milk Hot Chocolate",
+                    price: 2.99,
+                    quantity: 1,
+                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
+                },
+            ],
         },
         {
-            name: "Oat Milk Hot Chocolate",
-            price: 2.99,
-            quantity: 1,
-            image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
+            receipt_id: 123,
+            line_items: [
+                {
+                    name: "Oat Milk Hot Chocolate",
+                    price: 2.99,
+                    quantity: 1,
+                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
+                },
+            ],
+        },
+        {
+            receipt_id: 456,
+            line_items: [
+                {
+                    name: "Milk",
+                    price: 2.49,
+                    quantity: 2,
+                    image_url: "https://assets.sainsburys-groceries.co.uk/gol/1137637/1/640x640.jpg",
+                },
+            ],
+        },
+        {
+            receipt_id: 456,
+            line_items: [
+                {
+                    name: "Milk",
+                    price: 2.49,
+                    quantity: 1,
+                    image_url: "https://assets.sainsburys-groceries.co.uk/gol/1137637/1/640x640.jpg",
+                },
+            ],
         },
     ];
 }
