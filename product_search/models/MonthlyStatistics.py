ce5851637a49fe32c182c454e8955c19c5538d93
ocizevskis <115380988+ocizevskis@users.noreply.github.com>
2023-03-04 18:46:12 +0000

    Update MonthlyStatistics.py

--- product_search/models/MonthlyStatistics.py
+++ product_search/models/MonthlyStatistics.py
@@ -3,7 +3,13 @@ from typing import List
 
 @dataclass
 class MonthlyStatistics:
+    #these all are provisional, suggestions welcome
     monthlySpending: float
+    category_spending: list[tuple]
+    category_targets: list[tuple]
+    category_progress: list[tuple]
+    nectar_points: int
+    
     # blah blah blah
 
     def __init__(self, price, name_on_receipt): 
