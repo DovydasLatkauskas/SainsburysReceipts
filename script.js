56fc7441bbe939fcadc5c9c2443019016fcede55
Jeremy Foxcroft <foxcroftjn@gmail.com>
2023-03-05 08:54:03 +0000

    Start implementing history endpoint

--- script.js
+++ script.js
@@ -1,56 +1,15 @@
 async function getReceipts() {
-    return [
-        {
-            receipt_id: 123,
-            line_items: [
-                {
-                    name: "Waitrose Essential Oatmilk",
-                    price: 1.99,
-                    quantity: 1,
-                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/4/LN_718674_BP_4.jpg",
-                },
-                {
-                    name: "Oat Milk Hot Chocolate",
-                    price: 2.99,
-                    quantity: 1,
-                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
-                },
-            ],
+    var response = await fetch("http://localhost:8000/api/history", {
+        method: "GET",
+        mode: "no-cors",
+        headers: {
+            Accept: "application/json",
         },
-        {
-            receipt_id: 123,
-            line_items: [
-                {
-                    name: "Oat Milk Hot Chocolate",
-                    price: 2.99,
-                    quantity: 1,
-                    image_url: "https://ecom-su-static-prod.wtrecom.com/images/products/11/LN_595718_BP_11.jpg",
-                },
-            ],
-        },
-        {
-            receipt_id: 456,
-            line_items: [
-                {
-                    name: "Milk",
-                    price: 2.49,
-                    quantity: 2,
-                    image_url: "https://assets.sainsburys-groceries.co.uk/gol/1137637/1/640x640.jpg",
-                },
-            ],
-        },
-        {
-            receipt_id: 456,
-            line_items: [
-                {
-                    name: "Milk",
-                    price: 2.49,
-                    quantity: 1,
-                    image_url: "https://assets.sainsburys-groceries.co.uk/gol/1137637/1/640x640.jpg",
-                },
-            ],
-        },
-    ];
+    });
+    console.log(response);
+    // response = await response.json();
+    // console.log(JSON.stringify(response));
+    // return response;
 }
 
 function importData(data) {
